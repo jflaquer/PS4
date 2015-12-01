b2c4383b2241d085eeafeb913b294dc0de882fea
w1zgam3r <jflaquer@udel.edu>
2015-11-30 23:17:49 -0500

    Initial Commit

---
+++ src/ch/makery/ps4/model/PersonListWrapper.java
@@ -0,0 +1,23 @@
+package ch.makery.ps4.model;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import ch.makery.ps4.model.Person;
+
+@XmlRootElement(name = "persons")
+public class PersonListWrapper {
+
+    private List<Person> persons;
+
+    @XmlElement(name = "person")
+    public List<Person> getPersons() {
+        return persons;
+    }
+
+    public void setPersons(List<Person> persons) {
+        this.persons = persons;
+    }
+}
